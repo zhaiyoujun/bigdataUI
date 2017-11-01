d1d166d729d90d9dd0e4df50b7d67e7df9951cf0
zhaiyoujun <zhaiyoujun2016@163.com>
2017-11-01 22:42:24 +0800

    DBUtils .xml

--- olap/src/main/java/org/cas/ie/bigdata/olap_ui/controller/HelloSpringController.java
+++ olap/src/main/java/org/cas/ie/bigdata/olap_ui/controller/HelloSpringController.java
@@ -19,18 +19,9 @@ public class HelloSpringController {
 	@Autowired
     QueryService queryService;
 	
-    String message = "Welcome to Spring MVC!";
- 
-    @RequestMapping("/hello")
-    public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "Spring") String name) {
-    	 
-        ModelAndView mv = new ModelAndView("hellospring");//指定视图
-        //向视图中添加所要展示或使用的内容，将在页面中使用
-        mv.addObject("message", message);
-        mv.addObject("name", name);
-        return mv;
-    }
-    
+	@Autowired
+	DBUtils dbUtils;
+	
     @RequestMapping("/showdatabases")
     public ModelAndView shwoDatabases() throws ClassNotFoundException, SQLException, IOException {
      
@@ -38,7 +29,7 @@ public class HelloSpringController {
     	
     	String sql = "show databases;";
     	
-    	Connection conn = DBUtils.connect();
+    	Connection conn = dbUtils.connect(null);
     	
     	String rs = queryService.query(sql, conn);
     	
@@ -57,7 +48,7 @@ public class HelloSpringController {
     	
     	String sql = "show tables;";
     	
-    	Connection conn = DBUtils.connect(name);
+    	Connection conn = dbUtils.connect(name);
     	
     	String rs = queryService.query(sql, conn);
     	
--- olap/src/main/java/org/cas/ie/bigdata/olap_ui/sql/common/DBUtils.java
+++ olap/src/main/java/org/cas/ie/bigdata/olap_ui/sql/common/DBUtils.java
@@ -4,40 +4,42 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
-import org.cas.ie.bigdata.olap_ui.sql.common.PropertyUtils;
 
 public class DBUtils {
 
-	  public static Connection connect(Properties p) throws IOException, ClassNotFoundException, SQLException {
-	    Class.forName(p.getProperty("driver"));
+	private String driver = "com.mysql.jdbc.Driver";
+	private String dbconnect = "jdbc:mysql://127.0.0.1:3306";
+	private String username = "root";
+	private String password = "root";
+	
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+	
+	public void setDbconnect(String dbconnect) {
+		this.dbconnect = dbconnect;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	  public Connection connect(String database) throws IOException, ClassNotFoundException, SQLException {
+	    Class.forName(this.driver);
+	    String dbconn = this.dbconnect;
+	    if (database != null)
+	    	dbconn = dbconn + '/' + database;
 	    return DriverManager.getConnection(
-	      p.getProperty("dbconnect"),
-	      p.getProperty("username"),
-	      p.getProperty("password")
+	      dbconn,
+	      this.username,
+	      this.password
 	    );
 	  }
 
-	  public static Connection connect() throws IOException, ClassNotFoundException, SQLException {
-//	    return connect(PropertyUtils.get("db"));
-		Properties p= new Properties();
-		p.setProperty("driver", "com.mysql.jdbc.Driver");
-		p.setProperty("dbconnect", "jdbc:mysql://127.0.0.1:3306");
-		p.setProperty("username", "root");
-		p.setProperty("password", "root");
-		  
-	    return connect(p);	    
-	  }
-	  
-	  public static Connection connect(String database) throws IOException, ClassNotFoundException, SQLException {
-//		    return connect(PropertyUtils.get("db"));
-			Properties p= new Properties();
-			p.setProperty("driver", "com.mysql.jdbc.Driver");
-			p.setProperty("dbconnect", "jdbc:mysql://127.0.0.1:3306" + "/" + database);
-			p.setProperty("username", "root");
-			p.setProperty("password", "root");
-			  
-		    return connect(p);		    
-		  }
 	}
