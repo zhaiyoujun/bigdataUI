af3436749d9cc704740e13621f33153578ea9349
zhaiyoujun <zhaiyoujun2016@163.com>
2017-11-02 13:35:37 +0800

    show nodes

--- olap/src/main/java/org/cas/ie/bigdata/olap_ui/sql/common/ShellUtils.java
+++ olap/src/main/java/org/cas/ie/bigdata/olap_ui/sql/common/ShellUtils.java
@@ -3,15 +3,37 @@ package org.cas.ie.bigdata.olap_ui.sql.common;
 import java.util.ArrayList;
 
 public class ShellUtils {
-	private String host = "192.168.2.11,192.168.2.12,192.168.2.13";
+	private String host = "node1#192.168.2.11,node2#192.168.2.12,node3#192.168.2.13";
 	
 	public void setHost(String host) {
 		this.host = host;
 	}
 	
-	public String[] getIpSet() {
-		String[] ipSet = this.host.split(",");
-		return ipSet;
+	public ArrayList<String[]> splitHost() {
+		String[] hostSet = this.host.split(",");
+		
+		int hostCount = hostSet.length;
+		
+		String[] nodeSet = new String[hostCount];
+		String[] ipSet = new String[hostCount];
+		
+		for (int i = 0 ; i < hostCount; i++) {
+			String[] node = hostSet[i].split("#");
+			nodeSet[i] = node[0];
+			ipSet[i] = node[1];
+		}
+		ArrayList<String[]> splitResult = new ArrayList<String[]>();
+		splitResult.add(0, nodeSet);
+		splitResult.add(1, ipSet);
+		return splitResult;
+	}
+	
+	public String[] getNodeSet(ArrayList<String[]> splitResult) {
+		return splitResult.get(0);
+	}
+	
+	public String[] getIpSet(ArrayList<String[]> splitResult) {
+		return splitResult.get(1);
 	}
 	
 	public ArrayList<String> getPingCmdSet(String[] ipSet) {
