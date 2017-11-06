22d8b6276a819dea09a77f6e9c1f7427907f0a6a
zhaiyoujun <zhaiyoujun2016@163.com>
2017-11-06 19:56:39 +0800

    Storm host ip

--- storm-ui/src/main/java/org/cas/ie/bigdata/storm_ui/controller/HelloSpringController.java
+++ storm-ui/src/main/java/org/cas/ie/bigdata/storm_ui/controller/HelloSpringController.java
@@ -1,6 +1,9 @@
 package org.cas.ie.bigdata.storm_ui.controller;
 
+import org.cas.ie.bigdata.storm_ui.Utils.IpUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +11,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloSpringController {
+	
+	@Autowired
+	IpUtil ipUtil;	
+
     @RequestMapping("/")
     public ModelAndView showCluster() {
         ModelAndView mv = new ModelAndView("index");//指定视图
@@ -19,6 +26,11 @@ public class HelloSpringController {
         ModelAndView mv = new ModelAndView("topology");//指定视图
 //向视图中添加所要展示或使用的内容，将在页面中使用
         return mv;
+    }
+    
+    @RequestMapping("/getStorm_Host_Ip")
+    public String getStorm_Hosp_Ip() {
+    	return ipUtil.getStorm_Host_Ip();
     }
 	
 }
